321ce1bf1655f58b677d84e80b739c5eef64a928
mahdibohloul <mahdi_bohloul@yahoo.com>
2020-03-31 06:37:03 +0430

    Create user app and generate authentication for website and create user profile web page

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Mahdi',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 30, 2020'
-    },
-    {
-        'author': 'Erfan',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 29, 2020'
-    }
-
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home',
     }
     return render(request, 'blog/home.html', context)
---
+++ users/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        context = {
+            'form': form,
+            'title': 'Register',
+        }
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+        context = {
+            'form': form,
+            'title': 'Register',
+        }
+    return render(request, 'users/register.html', context)
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
